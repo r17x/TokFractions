88304da837c50e9d5d482bac512eddca9fdd16dc
ri7nz <ri7nz.labs@gmail.com>
2019-07-29 15:01:45 +0700

    changes: useFraction with Test update

--- src/useFraction.js
+++ src/useFraction.js
@@ -3,31 +3,32 @@
  * @description
  * The Fraction function with React Hook
  */
-import {useState} from 'react'
+import {useState, useCallback} from 'react'
 import fraction, {defaultFractioned, validationCurrency, getNumber} from './fractions'
 
 export default function useFractions(){
   const [amount, setAmount] = useState();
   const [result, setResult] = useState({
-      visible: false,
+    visible: false,
     fractioned: defaultFractioned, 
   })
-  const handleChange = (value, e) =>  {
-          e.persist()
-          setAmount(value)
-          setValid(validationCurrency(value))
-  }
-  
-  const handleKeyEnter = (e) => {
-      if(e.key === 'Enter' && valid && e.target.value){
-          setResult({
-            visible: !result.visible,
-            fraction: fraction(getNumber(amount))
-          })
-      }
-  }
   const [valid, setValid] = useState(null)
-  return [
+
+  const handleChange = useCallback((value) =>  {
+      setAmount(value)
+      setValid(validationCurrency(value))
+  },[])
+
+  const handleKeyEnter = useCallback((e) => {
+    if(e.key === 'Enter' && valid){
+        setResult({
+          visible: !result.visible,
+          fractioned: fraction(getNumber(amount))
+        })
+    }
+  },[amount, valid, result ]
+ )  
+    return [
       amount,
       result,
       valid,
